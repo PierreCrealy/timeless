f6f3435c3aedc7d2a8ed3ae35da32117a65d8c44
Pierre Bournazel <bourna13pi@lycee-ndduroc.com>
2024-12-15 12:41:05 +0100

    add new model , bdd and relations for hotel & activity

---
+++ app/Http/Controllers/RegisterActivityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRegisterActivityRequest;
+use App\Http\Requests\UpdateRegisterActivityRequest;
+use App\Models\RegisterActivity;
+
+class RegisterActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRegisterActivityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RegisterActivity $registerActivity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RegisterActivity $registerActivity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRegisterActivityRequest $request, RegisterActivity $registerActivity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RegisterActivity $registerActivity)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TypeActivityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTypeActivityRequest;
+use App\Http\Requests\UpdateTypeActivityRequest;
+use App\Models\TypeActivity;
+
+class TypeActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeActivityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeActivity $typeActivity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeActivity $typeActivity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeActivityRequest $request, TypeActivity $typeActivity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeActivity $typeActivity)
+    {
+        //
+    }
+}
